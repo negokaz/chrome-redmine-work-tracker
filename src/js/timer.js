bf9288f343d9c27ceca2cf1145a29dd5ff57dfd8
Kazuki Negoro <s63.kazuki.n@gmail.com>
2015-07-13 21:02:05 +0900

    Fix bug as can't start timer

--- src/js/timer.js
+++ src/js/timer.js
@@ -1,63 +1,88 @@
 var timer = {
 
+  status: {
+    /* タスクがなにもない状態(issueId が null になる) */
+    noTask: "noTask",
+    /* タイマーがカウントしている状態(時間エントリは未保存) */
+    running: "running",
+    /* 時間エントリが未保存で、タイマーが一時停止している状態 */
+    paused: "paused",
+    /* 時間エントリが保存済で、タイマーが停止している状態 */
+    stopped: "stopped"
+  },
+
   boot: function() {
     var self = this;
-    storage.get()
-    .then(function(data) {
-      if (!data.timer) {
-        return;
-      }
-      switch (data.timer.status) {
-        case 'started':
-          return self.onTimer(data.timer.issueId);
-      }
-    })
-    .then(function() {
-      self.setBadge();
-    })
-    .promise();
+    return storage.get()
+      .then(function(data) {
+        // インストール直後の起動
+        if (!data.timer) { return self.createTimerSchema(); }
+        // 2度目以降の起動
+        switch (data.timer.status) {
+          // カウント中にバックグラウンドの処理が落ちたときなどは
+          // タイマーを自動的に再開する
+          case 'running':
+            return self.onTimer(data.timer.issueId)
+              .then(function() { self.setBadge(data.timer.spendMillisec); });
+          default:
+            return new $.Deferred().resolve();
+        }
+      })
+      .promise();
+  },
+
+  createTimerSchema: function(issueId) {
+    var self = this;
+    return storage.set({
+        'timer': {
+          issueId: null,
+          status: self.status.noTask,
+          spendMillisec: 0
+        }
+      })
+      .promise();
   },
 
   start: function(issueId) {
     var self = this;
-    storage.get()
-    .then(function(data) {
-      if (!data.timer) {
-        self.upnew(issueId);
-      }
-      if (data.timer.issueId == issueId) {
-        switch (data.timer.status) {
-          case 'paused':
-            return self.resume();
-          default:
-            return self.renew(issueId);
-        }
-      } else {
-        return self.renew(issueId);
-      }
-    })
-    .promise();
-  },
-
-  upnew: function(issueId) {
-    return this.onTimer(issueId)
-      .then(function() {
-        return storage.set({
-          'timer': {
-            issueId: issueId,
-            status: 'started',
-            spendMillisec: 0
+    return storage.get()
+      .then(function(data) {
+        if (data.timer.issueId == issueId) {
+          switch (data.timer.status) {
+            case 'running':
+              return new $.Deferred().resolve().promise();
+            case 'paused':
+              return self.resume();
+            case 'stopped':
+              return self.renew(issueId);
           }
-        });
+        } else {
+          return self.renew(issueId);
+        }
       })
       .promise();
   },
 
   renew: function(issueId) {
     var self = this;
-    return this.stop()
-      .then(function() {
-        return self.upnew();
+    return storage.get()
+      .then(function(data) {
+        // まだ保存できていない場合は保存してから
+        if (data.timer.status == self.status.running
+            || data.timer.status == self.status.paused) {
+          return self.stop();
+        }
+      })
+      .then(storage.get)
+      .then(function(data) {
+        data.timer.issueId = issueId;
+        data.timer.status = self.status.running;
+        data.timer.spendMillisec = 0;
+        return data;
+      })
+      .then(storage.set)
+      .then(function(data) {
+        return self.onTimer(data.timer.issueId);
       })
       .promise();
   },
@@ -67,20 +92,21 @@ var timer = {
     var tickTimer = function() {
       return storage.get()
         .then(function(data) {
-          if (data.timer.status != 'started'
-              || (data.timer.status == 'started' && data.timer.issueId != issueId)) {
-            return new $.Deferred().reject();
-          }
+          // タイマーがカウントしている状態ではなくなった
+          if (data.timer.issueId == issueId && data.timer.status != self.status.running) { return new $.Deferred().resolve(); }
+          // タスクが変わった
+          if (data.timer.issueId != issueId && data.timer.status == self.status.running) { return new $.Deferred().resolve(); }
+
+          // タイマーを進める
           data.timer.spendMillisec += 1000;
-          return storage.set(data);
-        })
-        .then(function(data) {
-          self.setBadge(data.timer.spendMillisec);
-        })
-        .then(function() {
-          setTimeout(tickTimer, 1000);
-        }, function() {
-          return new $.Deferred().resolve();
+          return storage.set(data)
+            .then(function(data) {
+              console.log(data);
+              self.setBadge(data.timer.spendMillisec);
+            })
+            .then(function() {
+              setTimeout(tickTimer, 1000);
+            });
         });
     };
     return tickTimer().promise();
@@ -90,7 +116,7 @@ var timer = {
     var self = this;
     return storage.get()
       .then(function(data) {
-        data.timer.status = 'paused';
+        data.timer.status = self.status.paused;
         return data;
       })
       .then(storage.set)
@@ -104,7 +130,7 @@ var timer = {
     var self = this;
     return storage.get()
       .then(function(data) {
-        data.timer.status = 'started';
+        data.timer.status = self.status.running;
         return data;
       })
       .then(storage.set)
@@ -116,10 +142,10 @@ var timer = {
 
   stop: function() {
     var self = this;
-    return this.postTime()
+    return self.postTime()
       .then(storage.get)
       .then(function(data) {
-        data.timer.status = 'stoped';
+        data.timer.status = self.status.stopped;
         data.timer.spendMillisec = 0;
         return data;
       })
@@ -131,18 +157,20 @@ var timer = {
   },
 
   postTime: function(onSuccess) {
-    var post = function(data) {
-      return $.post(data.options.redmineRootUrl + "time_entries.json", {
-        key: data.options.apiAccessKey,
-        time_entry: {
-          issue_id: data.timer.issueId,
-          // ミリ秒 を 時間に換算
-          hours: data.timer.spendMillisec / (60 * 60 * 1000),
-          comments: ""
-        }
-      });
-    }
-    return storage.get().then(post).promise();
+    var self = this;
+    return storage.get()
+      .then(function(data) {
+        return $.post(data.options.redmineRootUrl + "time_entries.json", {
+          key: data.options.apiAccessKey,
+          time_entry: {
+            issue_id: data.timer.issueId,
+            // ミリ秒 を 時間(hour)に換算
+            hours: data.timer.spendMillisec / (60 * 60 * 1000),
+            comments: ""
+          }
+        });
+      })
+      .promise();
   },
 
   setBadge: function(timeMillisec) {
