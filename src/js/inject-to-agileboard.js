19d76a9af9b7cbab4cc6a9a0db3fab15ef17a645
negokaz <negokaz@gmail.com>
2015-07-16 00:19:28 +0100

    Fix agile support

--- src/js/inject-to-agileboard.js
+++ src/js/inject-to-agileboard.js
@@ -1,13 +1,13 @@
 $(function() {
   'use strict';
 
-  var $startTag = $('<a class="icon icon-test redmine-timer-start" style="float:right;color:#888;" href="javascript:void(0)">Do</a>');
+  var $startTag = $('<p class="time-tracker" style="text-align:right;"><a class="icon icon-test redmine-timer-start" style="color:#888;" href="javascript:void(0)">Do</a></p>');
 
-  $startTag.appendTo('.issue-card > .fields > .info');
+  $startTag.appendTo('.issue-card > .fields');
 
   $('a.redmine-timer-start').on('click', function() {
     var issueId = $(this).parents(".issue-card").attr("data-id");
-    chrome.runtime.sendMessage({method: "start-task", param: {issueId: issueId}, function(response) {
+    chrome.runtime.sendMessage({method: "start-task", param: {issueId: issueId}}, function(response) {
       if (response.success) { location.reload(); }
     });
   });
