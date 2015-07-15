6eae4ef040e6d4124aaefde989ea1caa712db1ef
Takashi Sugimoto <sugimoto.takashi@tis.co.jp>
2015-07-15 14:33:00 +0900

    inject-to-redmine.jsの実行方法の効率化

--- src/js/background.js
+++ src/js/background.js
@@ -1,29 +1,25 @@
-function loadOption() {
-  chrome.storage.local.get(function(data) {
-     if (data.options) {
-        chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-          chrome.tabs.query({
-            url: data.options.redmineRootUrl + 'issues/*'
-          }, function (tabs) {
-            $.each(tabs, function(i, tab) {
-              if (tab.id == tabId) {
-                chrome.tabs.executeScript(tabId, {file: 'js/jquery.js'});
-                chrome.tabs.executeScript(tabId, {file: 'js/inject-to-redmine.js'});
-              }
-            });
-          });
-        });
-     }
-  });
-}
-loadOption();
+var options = {};
+
+chrome.storage.local.get(function(data) {
+    options = data.options;
+});
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    if (options && options.redmineRootUrl && changeInfo.status === "complete" && tab.url.indexOf(options.redmineRootUrl + 'issues/') === 0) {
+        chrome.tabs.executeScript(tabId, {file: 'js/jquery.js'});
+        chrome.tabs.executeScript(tabId, {file: 'js/inject-to-redmine.js'});
+    }
+});
+
+chrome.storage.onChanged.addListener(function (changeInfo, type){
+    if (type === "local" && changeInfo.options) {
+        options = changeInfo.options.newValue;
+    }
+});
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     switch(request.method) {
-      case 'load-option':
-        loadOption();
-        break;
       case 'start-timer':
         timer.start(request.param.issueId);
         break;
