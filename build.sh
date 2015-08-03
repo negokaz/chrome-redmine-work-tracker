d3157c285022a422796b6b78e831523193f00a52
Kazuki Negoro <s63.kazuki.n@gmail.com>
2015-08-03 20:25:45 +0900

    Implemented option validation and cosme option view

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
-mkdir -p dist/js
-mkdir -p dist/css
-mkdir -p dist/icon
-
-cp src/manifest.json  dist/
-cp src/js/*.js        dist/js/
-cp icon/*.png         dist/icon/
-
-cp bower_components/jquery/dist/jquery.js dist/js/
-
-vulcanize src/popup.html   --inline-script --inline-css | crisper --html dist/popup.html   --js dist/popup.js
-vulcanize src/options.html --inline-script --inline-css | crisper --html dist/options.html --js dist/options.js
-
-cd dist/; zip -r redmine-time-tracker.zip *
+mkdir -p dist/js \
+&& mkdir -p dist/css \
+&& mkdir -p dist/icon \
+\
+&& cp src/manifest.json  dist/ \
+&& cp src/js/*.js        dist/js/ \
+&& cp icon/*.png         dist/icon/ \
+\
+&& cp bower_components/jquery/dist/jquery.js dist/js/ \
+\
+&& vulcanize src/popup.html   --inline-script --inline-css | crisper --html dist/popup.html   --js dist/popup.js \
+&& vulcanize src/options.html --inline-script --inline-css | crisper --html dist/options.html --js dist/options.js \
+\
+&& cd dist/ && zip -r redmine-time-tracker.zip *
