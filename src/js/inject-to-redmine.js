b86fbda14ffa494c4731ecb07a3522982f8b5ab3
negokaz <negokaz@gmail.com>
2017-01-22 17:29:00 +0900

    Fix reminder

--- src/js/inject-to-redmine.js
+++ src/js/inject-to-redmine.js
@@ -2,12 +2,17 @@ $(function() {
   'use strict';
 
   function enableWorkReminder() {
-    // top-menuの前に追加してしまうと、↓スクロールしたときにヘッダに隠れてしまうのでbodyの最後に追加する
-    var $reminderTag = $("<span style='width:100%;height:1.8em;text-align:center;padding-top:2px;background:red;color:white;position:fixed;top:0;'>実施中のタスクがありません</span>").hide();
-    $reminderTag.appendTo('body');
-    // top-menuが隠れてしまわないように
-    var $marginTag = $("<div style='height:1.8em;'></div>").hide();
-    $marginTag.insertBefore("#top-menu");
+
+    var $reminderTag = $("#__redmine_timetracker_reminder");
+    if ($reminderTag.length == 0) {
+      $reminderTag = $("<span id='__redmine_timetracker_reminder' style='width:100%;height:1.8em;text-align:center;padding-top:2px;background:red;color:white;position:fixed;top:0;'>実施中のタスクがありません</span>").hide();
+      $reminderTag.appendTo('body');
+    }
+    var $marginTag = $("#__redmine_timetracker_reminder_margin");
+    if($marginTag.length == 0) {
+      $marginTag = $("<div id='__redmine_timetracker_reminder_margin' style='height:1.8em;'></div>").hide();
+      $marginTag.insertBefore("#top-menu");
+    }
 
     function displayAlert() {
       $marginTag.show();
