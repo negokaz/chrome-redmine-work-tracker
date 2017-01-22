f45d35ef2e4b53b1bee63799781d5bd7ecb6d2fb
negokaz <negokaz@gmail.com>
2017-01-22 21:40:55 +0900

    Implemented countdown timer

--- src/js/timer.js
+++ src/js/timer.js
@@ -13,7 +13,15 @@ var timer = {
 
   _tickInterval: null,
 
+  _startRemainingMillisecInterval: function() {
+    setInterval(function() {
+      this._updateRemainingMillisec(this._timerData);
+    }.bind(this), 60000);
+  },
+
   _timerData: null,
+
+  _trackingIssue: null,
 
   _bihavior: null,
 
@@ -46,7 +54,7 @@ var timer = {
     }.bind(this)).then(function() {
       return storage.set({ timer: this._timerData });
     }.bind(this)).then(function() {
-      this._setBadge(this._timerData.spendMillisec);
+      this._setBadge(this._timerData);
     }.bind(this));
   },
 
@@ -190,6 +198,7 @@ var timer = {
   },
 
   boot: function() {
+    this._startRemainingMillisecInterval();
     return storage.get()
       .then(function(data) {
         // インストール直後の起動
@@ -206,6 +215,8 @@ var timer = {
           this._changeBehavior(data.timer);
           return this._process({ command: "boot" });
         }
+      }.bind(this)).then(function() {
+        return this._updateRemainingMillisec(this._timerData);
       }.bind(this));
   },
 
@@ -216,7 +227,8 @@ var timer = {
           issueId: null,
           status: self.status.noTask,
           spendMillisec: 0,
-          latestTickTime: 0
+          latestTickTime: 0,
+          remainingMillisec: null
         }
       });
   },
@@ -230,7 +242,9 @@ var timer = {
   },
 
   start: function(issueId) {
-    return this._process({ command: "start", issueId: issueId });
+    return this._process({ command: "start", issueId: issueId }).then(function() {
+      return this._updateRemainingMillisec(this._timerData);
+    }.bind(this));
   },
 
   pause: function() {
@@ -276,15 +290,60 @@ var timer = {
       }.bind(this));
   },
 
-  _setBadge: function(timeMillisec) {
-    if (timeMillisec) {
-      var date = new Date(timeMillisec);
-      var formatedSpendTime =
-        date.getUTCHours() + ":" + ('0' + date.getUTCMinutes()).slice(-2);
-      chrome.browserAction.setBadgeText({text: formatedSpendTime});
-    } else {
-      chrome.browserAction.setBadgeText({text: ""});
+  _updateRemainingMillisec: function(timerData) {
+    if (!timerData.issueId) {
+      // do nothing
+      return $.Deferred().resolve();
     }
+    return storage.get()
+      .then(function(data) {
+        if (data.options.countdown) {
+          return $.getJSON(data.options.redmineRootUrl + "issues/" + timerData.issueId + ".json", {
+            key: data.options.apiAccessKey
+          }).then(function(response) {
+            if (response.issue && response.issue.estimated_hours && response.issue.spent_hours) {
+              timerData.remainingMillisec = (response.issue.estimated_hours - response.issue.spent_hours) * 60 * 60 * 1000;
+            } else {
+              timerData.remainingMillisec = null;
+            }
+            return timerData;
+          }.bind(this));
+        } else {
+          timerData.remainingMillisec = null;
+          return $.Deferred().resolve(timerData);
+        }
+      }.bind(this));
+  },
+
+  _setBadge: function(timerData) {
+    var spendMillisec = timerData.spendMillisec;
+
+    var formatedSpendTime = null;
+    var colorR = 0;
+    var colorG = 0;
+    var colorB = 0;
+
+    if (spendMillisec) {
+      var date = null;
+      if (timerData.remainingMillisec) {
+        date = new Date(Math.abs(timerData.remainingMillisec - spendMillisec));
+        var colorWeightPer = (timerData.remainingMillisec > 0 && timerData.remainingMillisec > spendMillisec)
+          ? spendMillisec / timerData.remainingMillisec
+          : 1.0;
+        colorR = Math.floor(255 * colorWeightPer);
+        colorG = Math.floor(255 * (1.0 - colorWeightPer));
+        colorB = 0;
+      } else {
+        date = new Date(spendMillisec);
+        colorR = 0;
+        colorG = 0;
+        colorB = 255;
+      }
+      formatedSpendTime =
+        date.getUTCHours() + ":" + ('0' + date.getUTCMinutes()).slice(-2);
+    }
+    chrome.browserAction.setBadgeText({text: (formatedSpendTime ? formatedSpendTime : "")});
+    chrome.browserAction.setBadgeBackgroundColor({color: [colorR, colorG, colorB, 255]});
   }
 };
 
